943e5bb58c580974896c1e1a086723724c8777b5
Joseph Vunanga <vunangajoseph@gmail.com>
2024-09-18 20:18:40 +0200

    E-Afya Firebase Initialization

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 // void main() {
 //   runApp(const MyApp());
 // }
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'E-Afya Platform',
       theme: ThemeData(
         // This is the theme of your application.
         //
